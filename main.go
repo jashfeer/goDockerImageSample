d04d1d24b757eaa32fa522a25e61622a36bb6d68
Jashfeer Abdullah <jashfeerabdullah@gmil.com>
2022-08-13 07:30:05 +0530

    handler added

--- main.go
+++ main.go
@@ -1,6 +1,18 @@
 package main
 
-func main(){
+import (
+	"html/template"
+	"net/http"
+)
 
+
+func index(res http.ResponseWriter, req *http.Request) {
+	tmp, _ := template.ParseFiles("index.gohtml")
+	tmp.Execute(res, nil)}
+
+func main(){
+	http.HandleFunc("/", index)
+
+	http.ListenAndServe(":9090",nil)
 	
 }
