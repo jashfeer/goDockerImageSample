b60787b5c7b1c5ac418ccac6bf02b4d467976f5c
Jashfeer Abdullah <jashfeerabdullah@gmil.com>
2022-08-13 08:15:49 +0530

    docker image started

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
 
-
 func index(res http.ResponseWriter, req *http.Request) {
-	tmp, _ := template.ParseFiles("index.gohtml")
-	tmp.Execute(res, nil)}
-
-func main(){
-	http.HandleFunc("/", index)
-
-	http.ListenAndServe(":9090",nil)
 	
+	tmp, _ := template.ParseFiles("index.gohtml")
+	tmp.Execute(res, nil)
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	fmt.Println("Started..")
+	http.ListenAndServe(":9090", nil)
+
 }
